95566e4524a83f23b694716c0b9484417b91c60f
Venkatesh <venkateshnalla94@gmail.com>
2020-10-08 01:28:09 +0530

    Reducers and actions in counters

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,13 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState,useReducer} from 'react';
 import { Text, StyleSheet,View, Button,TouchableOpacity} from "react-native";
 
-const CounterScreen=()=>{
+const reducer=(state,action)=>{
+     switch (action.type) {
+          case 'Increase_counter':
+               return {...state,counter:state.counter+action.payload};
+          case 'Decrease_counter':
+               return {...state,counter:state.counter+action.payload};
+          default: return state
      
-     const [counter,setCounter]=useState(0);
+     }
+}
+
+const CounterScreen=()=>{
+     const [state,dispatch]=useReducer(reducer,{counter:0})
+     const {counter}=state;
      return(
           <View>
-               <Button title="Increase" onPress={()=>{setCounter(counter+1)}}/>
-               <Button title="Decrease" onPress={()=>{setCounter(counter-1)}}/>
+               <Button title="Increase" onPress={()=>{
+                    dispatch({type:'Increase_counter',payload:1})
+               }}/>
+               <Button title="Decrease" onPress={()=>{
+                    dispatch({type:'Decrease_counter',payload:-1})
+               }}/>
                <Text>{counter}</Text>
           </View>
      )
